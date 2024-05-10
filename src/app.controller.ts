d2babc1977ceaf2530d8bc1cfdc9113851d1718d
AcidProTech <asad1gohar@gmail.com>
2024-05-10 16:39:37 +0500

    added firebase notfication

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,59 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import admin from 'firebase-admin';
+import { firebase_config } from './utils/firebase.util';
 
 @Controller()
 export class AppController {
+  private readonly frebase_admin = admin.initializeApp({
+    credential: admin.credential.cert(firebase_config),
+  });
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/request')
+  async create(
+    @Body('tokens') tokens: string[],
+    @Body('title') title: string,
+    @Body('body') body: string,
+  ) {
+    try {
+      if (!tokens || !title || !body) {
+        return {
+          message: 'Missing Required Fields',
+        };
+      }
+
+      const message = {
+        notification: {
+          title,
+          body,
+        },
+        tokens,
+      };
+
+      const send_notification = await this.frebase_admin
+        .messaging()
+        .sendEachForMulticast(message);
+      if (send_notification) {
+        return {
+          message: 'Notification sent',
+          data: message,
+          error: false,
+        };
+      }
+      return {
+        message: 'Something went wrong from server side.',
+        data: message,
+        error: true,
+      };
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
---
+++ src/utils/firebase.util.ts
@@ -0,0 +1,13 @@
+export const firebase_config = {
+  "type": "service_account",
+  "projectId": "amsy-7b201",
+  "private_key_id": "81ad2711ab593e0f671ce1a9c3ee1ad9ec73c934",
+  "privateKey": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCjuslVkJNbxsUE\nbsZNcvjrDylqFQ7NPKajjJKpyN9cWWEGo53+/kWbPzwnPtWX3GEIO1taisPIkw6a\nN7+/Z68++3vlCjP6LF3WHOvD/9eWACKCO98nSIAGFMb+vJOK76cvN2J3Z0CNENgm\nzXwE/Fwu/gsYuj8Ni3XRT8F1N3g0jYe2zvtVbM9gVUc1ZxVYpYmg9I7xttr4ibe9\nM/gTc2N0UPbKCZAUHrAGZJunCD6XZCI40dGdt55VpS74HTVaPXAoSKoDJFop9o/p\nVtvdEzZVEgmEeagNX16oJi2GAO1stGBO4xZeVE8L4r5cDQbzYMEaKzJEvSNzJoA7\nSneT6QfLAgMBAAECggEAAKMQn8YJe5e8k5TJAsk9cmfrEzas7V4CtgHku0GWdW+d\nPsfD6Nt1m4LzH2BRcWJ3q2AzooIW+D8+rOZ55cd3wqdLcRBBg6KPDDUzBV1u+S7j\ntk+/SFpvQrwtKWl7jVJe7+8SCDXKfd7jdoxQvg+WBZrwj6JXPUn56ryOkFb77FGv\nbtCgvYWtkj5k7sN1cHFodu0JSATqFedvj6gspVxPi4P7C9412789abb2+EDVF+j5\nxMJCP3picLRuRxYgMwzjNgCa1h8KjSxlkhrjytGx+KpwnYYH/o8SSIJFOao0442X\nF4FuEZywPBunkNjxAkg9yt0zmtIwMVvwiuHAt3+RGQKBgQDXouhYIgS9xYj1O8kv\nOEuVWfWmuhBADex0QcdVZlSKf65+GVZwmMBw9WfaaJvu0n7C7fAthm+VBzmaxUlH\nPDOmMeHCSvkw6U/t/0b1SqRXOWXgfKLXQuxyHHGqs4cpr2jN35Eddz+lStJckQSc\n7P0r509u7pcY+UDJb3E7ipq9VwKBgQDCYI6qGu72oH4ntVIiE+VsxpzjMdj4Nblj\n2QXqyXJQnQCb3mpb4kLg2segHPRh7pamR1ynYpc1SecRvyRwXuUfaMrOhQx/RVCB\nLIQwxhe1+Fk1FuG46NDQzNSvkDxui/PtNeuV5xftQd0chjP2y6K13MyvPdCOs9cG\n/5FYWeEMrQKBgA2QB4ETkqzE8dpKylqX9dkM8XBtH3yH46lkQb9TUlnrsfPocBFg\n6imwpbmM/tHEl4h3IrYaUIZVEAB2DCfY137KgGKSCJ5U7lwPxEzFRxTdXVUDQdK8\nONbshxGvfP7YM4VPXHc3+t7Ql+6OiHW+SiYqxEo743EErFh/E+Ykz5q9AoGAD6wB\nVK2/PptKEKGJ8b1w7ghFdfv65mhOfkJtO7TqRp2iAu833BiuE7sQBVTQT2UROG7g\nhY5pap5+xuuGYJkGGDY2w9fuXgSUrRWNwvD10HIpnyItKN98lpktPMqAVNlr3WBP\nklKDl1mMHlSjs+5PPCKFTkvJmPffCm1oppr4jjECgYALHCSY/5weo9eScahN0e8R\ncasr4uqSESqmwQQppaWEKVxh7S5RDhyz5gzofJWDq5WnoLE/fUT1yYFQEDVoW4RO\nK7ncqiyUNGOQLbTrAyl6HuoHTSOQlMOrn7A17Ch5/FWiervm9XUsjil/FXgNb8jy\ngE7f7ZY2yyMVZH5LY4CFbQ==\n-----END PRIVATE KEY-----\n",
+  "clientEmail": "firebase-adminsdk-grc2o@amsy-7b201.iam.gserviceaccount.com",
+  "client_id": "111697618521735008885",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-grc2o%40amsy-7b201.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
